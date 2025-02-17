e80689a184c62e5326c03ec43a484a4ec64bf830
Darareach In <Darareach.In@mjqe.edu.kh>
2025-02-18 04:54:06 +0700

    update

--- build/index.js
+++ build/index.js
@@ -9,15 +9,35 @@ document.addEventListener("DOMContentLoaded",function ( ){
     ul.className = "list-container";
     container.insertBefore(ul,countTask)
     addBtn .addEventListener("click",addTask);
+    ul.addEventListener("click",handleTask)
 }); 
 
 function addTask (event){
-        event.preventDefault();       
-    const input = document.querySelector("input")
-    const li = document.createElement("li");
-    console.dir(input,li)
+    event.preventDefault();      
+    const ul = document.querySelector(".list-container")
+    let input = document.querySelector(".input");
+    const newTask = input.value.trim();
+    if(!newTask){
+        alert("Add Task!");
+    }else {
+        const li = document.createElement("li");
+        li.innerHTML = `${input.value}<i class="fa-solid fa-circle-xmark"></i>`;
+        ul.appendChild(li);
+    }
+    input.value = "";
 }
 
+function handleTask(event) {
+    if(event.target.tagName === "I"){
+        event.target.parentElement.remove();
+    }
+    if(event.target.tagName === "LI"){
+        event.target.classList.toggle ("completed");
+    }
+}
 
+function clearAllTasks (){
+    console.log("clear");
+}
 
 
