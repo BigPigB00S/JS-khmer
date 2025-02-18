0df492e055ec626999b561e56ec34918f2c3c8bc
BigPigB00S <bpig47575@gmail.com>
2025-02-19 00:53:38 +0700

    114.tagged template literal

--- build/index.js
+++ build/index.js
@@ -1,1 +1,13 @@
 // ES6 (Big Update 2015)
+//Template & tagged template literal
+
+function tagFunC(str,n,a) {
+    const checkAge = a > 17 ? "Adult" : "a monor";
+    return `${str[0]}${n}${str[1]} ${a} ${checkAge}`;
+}
+const name = "Bigpig";
+const age = 30;
+
+const print = tagFunC`hello ${name} you're ${age}`;
+
+console.log(print);
