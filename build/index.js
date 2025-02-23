971756f708019b874247972efa1f0bce8753e2d4
Darareach In <Darareach.In@mjqe.edu.kh>
2025-02-24 05:00:20 +0700

    126.Object-spread

--- build/index.js
+++ build/index.js
@@ -1,21 +1,11 @@
 // ES6 (Big Update 2015)
-//array-spread operator
+//Object -spread operator
 
-const buttons = document.querySelectorAll("button");
+const car = {
+    brand: "BMW",
+    model: "6 Series"
+};
 
-<<<<<<< HEAD
-for (const phone of phones){
-    if (phone === "OneMe") continue;
-    console.log(phone);
-}
+const myCar = {...car};
 
-//continue: mean skip
-//break: 
-=======
-const allButton = [...buttons];
-const allButton = buttons.map(function (btn){
-    return btn;
-});
-
-console.log(allButton);
->>>>>>> 60f8a83908aba15dce2ad35ce8e2c11bf3ff4396
+console.log(myCar,car);
