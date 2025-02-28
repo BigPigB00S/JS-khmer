8038210a4606302d9443929e5b2f49a5babccdb5
Darareach In <Darareach.In@mjqe.edu.kh>
2025-02-28 16:17:49 +0700

    create website

--- build/index.js
+++ build/index.js
@@ -1,20 +1,5 @@
 // ES6 (Big Update 2015)
-//array methods (more)
-
-const myArray = ["a","b","c","d"];
-
-const result = myArray.includes("a");
-
-console.log(result)
-
-//find: find value
-//findIndex :mean find index
-//every: mean is the same ban vea jenh true
-//same: it have and than true
-
-
-    
-
+//Exercise (truist place )
 
 
 
