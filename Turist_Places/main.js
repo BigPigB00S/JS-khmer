881d10aa9c8725fe0c9b06d98993e5ac7f1c212a
Darareach In <Darareach.In@mjqe.edu.kh>
2025-03-03 17:00:40 +0700

    js_script

---
+++ Turist_Places/main.js
@@ -0,0 +1,10 @@
+//Exercise (truist place )
+
+const turistPlaces = [
+    {
+        id: 1,
+        country: "Cambodia",
+        title: "Angkor Wat",
+        img:"./pics/Cambodia/angkor-wat-425689_1280.jpg"
+    }
+]
